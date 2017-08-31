e054f13c0889ec38da6ab7effa1da5aa1146d505
MrFuentastic <vfuentes8587@gmail.com>
2017-08-31 08:11:24 -0500

    completed Thursday's daily problem

---
+++ week_2/Thursday.rb
@@ -0,0 +1,20 @@
+def split_array(arr, split)
+  new_arr = []
+  arr_counter = 0
+  new_arr_counter = 0
+    while arr_counter < arr.length
+      new_arr[new_arr_counter] = []
+      split.times do
+        if arr[arr_counter] == nil
+          break
+        else
+        new_arr[new_arr_counter] << arr[arr_counter]
+        arr_counter += 1
+        end
+      end    
+      new_arr_counter += 1
+    end
+  return new_arr
+end
+
+p split_array([0,1,2,3,4,5], 5)
