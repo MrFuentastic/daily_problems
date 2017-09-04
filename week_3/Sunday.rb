bfbf922270957bfc7d74ca27832c0e9a02fcb32c
MrFuentastic <vfuentes8587@gmail.com>
2017-09-04 13:32:24 -0500

    completed Sunday's daily problem

---
+++ week_3/Sunday.rb
@@ -0,0 +1,8 @@
+# Read about the Ruby `reduce` method. Then refactor the code below using `reduce`.
+numbers = [1, 2, 4, 2]
+sum = 0
+# numbers.each do |number|
+  # sum += number
+# end
+# p sum
+p numbers.reduce(:+)
