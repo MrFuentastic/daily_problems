0c91807556c43ad181bbf112ef6893b4d42dca4d
MrFuentastic <vfuentes8587@gmail.com>
2017-09-04 13:48:39 -0500

    completed Monday's daily problem

--- week_3/Monday.rb
+++ week_3/Monday.rb
@@ -1,10 +1,10 @@
-Write a method called `scrabble_score` which should accept a string as a parameter and return the amount of points it is worth in Scrabble. You can use the following hash to determine how many points each letter is worth:
-Use the `reduce` method to perform the computation in your scrabble_score method. The code should work as follows:
+# Write a method called `scrabble_score` which should accept a string as a parameter and return the amount of points it is worth in Scrabble. You can use the following hash to determine how many points each letter is worth:
+# Use the `reduce` method to perform the computation in your scrabble_score method. The code should work as follows:
 
 def scrabble_score(string)
   points = {'a'=> 1, 'b'=> 3, 'c'=> 3, 'd'=> 2, 'e'=> 1, 'f'=> 4, 'g'=> 2, 'h'=> 4, 'i'=> 1, 'j'=> 8, 'k'=> 5, 'l'=> 1, 'm'=> 3, 'n'=> 1, 'o'=> 1, 'p'=> 3, 'q'=> 10, 'r'=> 1, 's'=> 1, 't'=> 1, 'u'=> 1, 'v'=> 4, 'w'=> 4, 'x'=> 8, 'y'=> 4, 'z'=> 10}
 
-  string.split('').reduce
+  string.split('').reduce(0){|sum, letter| sum += points[letter]}
 
 end
 
