eed1d4112007eacc37015a48e3cc016af019f376
MrFuentastic <vfuentes8587@gmail.com>
2017-09-19 15:50:07 -0500

    Completed Monday's daily problem

--- week_5/Monday.rb
+++ week_5/Monday.rb
@@ -4,6 +4,4 @@ sentences = ["hello", "old friend", "today", "mac and cheese"]
 # into a new array of sentences where the first letter is capitalized only if there is more than one word in the sentence. The result should be:
 # ["hello", "Old friend", "today", "Mac and cheese"]
 
-# sentences.map{|string| string =~ /\s/ ? "#{string[0].upcase}#{string[1]}
-
-p sentences[0][1]
+p sentences.map{|string| string =~ /\s/ ? "#{string[0].upcase}#{string[1..-1]}" : string}
