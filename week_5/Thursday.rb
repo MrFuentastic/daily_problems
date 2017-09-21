497c9ce82260e7d4865b5bb0463ead0e879dbf08
MrFuentastic <vfuentes8587@gmail.com>
2017-09-20 20:29:26 -0500

    completed

--- week_5/Thursday.rb
+++ week_5/Thursday.rb
@@ -1,11 +1,14 @@
 # Write a method called `find_the_most_letter_a` which takes in an array of strings and returns the string with the most a’s.
 
 def find_the_most_letter_a(arr_a)
+  
+  bird = ""
   arr_a.each do |word|
-    p word.match(/a{0,}/)
+    p bird
+    bird = word if word.scan(/a/).length > bird.scan(/a/).length
   end
+
+  return bird
 end
-
-
 
 p find_the_most_letter_a(["alphabet", "aardvark", "anarchy"])  # => "aardvark"
