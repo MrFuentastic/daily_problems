903bb80b54fe62951b5bb72843d0b55fcaed993d
MrFuentastic <vfuentes8587@gmail.com>
2017-09-25 17:58:30 -0500

    Started Monday's daily problem

---
+++ week_6/Monday.rb
@@ -0,0 +1,7 @@
+# Use the `scan` method with a regular expression to find every word in the following sentence that has 4 letters and starts with the letter "l" and ends with the letter "e".
+sentence = "You can live like a king but make sure it isn't a lie."
+
+# The result should be:
+# ["live", "like"]
+
+p sentence.scan(/\w+/){|word| puts word if word.match(/^l/)}
